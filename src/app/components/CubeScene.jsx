b23520d9431efba36b4165435722908d950defa3
AndresCRomo <andres.calderon@miaa.mx>
2025-06-05 15:55:06 -0600

    Nuevamente cambios en posicionamiento de cubos

--- src/app/components/CubeScene.jsx
+++ src/app/components/CubeScene.jsx
@@ -125,7 +125,7 @@ function SpinningCube({ position, color }) {
 
 export default function CubeScene() {
 return (
-    <div className="fixed md:-top-1/7 -top-1/6  left-0 w-full h-full -z-10">
+    <div className="fixed lg:-top-[14%]  -top-1/6 md:-top-[22%]  left-0 w-full h-full -z-10">
         <Canvas camera={{ position: [0, 0, -5], fov: 75, near: 0.1, far: 1000 }} resize={{scroll:true}}>
         <ambientLight intensity={0.5} />
         <directionalLight position={[5, 5, 5]} />
